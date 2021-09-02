b3a76589e0e2ebb67b7cee0f6f5fd997a0aa14e3
unknown <rasedk434@gmail.com>
2021-09-02 14:20:27 +0600

    ffff

--- api.js
+++ api.js
@@ -29,7 +29,6 @@ const displaySearchResult=books=>{
    } 
    else{
     books.forEach(book=> {
-      const cover_i = searchField.value;
       const div = document.createElement('div');
       div.classList.add('col');
       div.innerHTML=`
